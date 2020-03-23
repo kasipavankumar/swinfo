3699f3a2e8ebdcf332f7525e71c987ed15b14d03
Code-Plus-Coffee <m416a@protonmail.com>
2020-03-23 22:14:06 +0530

    Add caching

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import DataCard from "../Card/Card";
+import DismissibleAlert from "../Alert/Alert";
 import { CircularProgress } from "@material-ui/core";
 
 import "./CardList.scss";
@@ -8,7 +9,8 @@ class CardList extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            Cards: []
+            Cards: [],
+            hasErrors: false
         };
     }
 
@@ -19,37 +21,70 @@ class CardList extends Component {
     componentWillUnmount() {
         this.setState({
             Cards: []
-        })
+        });
     }
 
     CardList = async () => {
         const { endpoint } = this.props;
-        const results = await getData(endpoint);
-
-        this.setState({
-            Cards: results.map((result, i) => (
-                <DataCard key={i} endpoint={endpoint} data={result} />
-            ))
-        });
+        const results = await cacheData(endpoint);
+        if (results !== "error") {
+            this.setState({
+                Cards: results.map((result, i) => (
+                    <DataCard key={i} endpoint={endpoint} data={result} />
+                ))
+            });
+        } else {
+            this.setState({ hasErrors: true });
+        }
     };
 
     render() {
-        const { Cards } = this.state;
+        const { Cards, hasErrors } = this.state;
 
-        return Cards.length === 0 ? (
+        if (hasErrors) {
+            return (
+                <DismissibleAlert
+                    severity="error"
+                    message="Error loading data, try again later."
+                />
+            );
+        } else {
+            if (Cards.length === 0) {
+                return (
+                    <div className="circularProgressIndicator">
+                        <CircularProgress />
+                    </div>
+                );
+            } else {
+                return <div className="cards__grid">{Cards}</div>;
+            }
+        }
+
+        /* return Cards.length === 0 ? (
             <div className="circularProgressIndicator">
                 <CircularProgress />
             </div>
         ) : (
             <div className="cards__grid">{Cards}</div>
-        );
+        ); */
     }
 }
 
-async function getData(endpoint) {
-    const response = await fetch(`https://swapi.co/api/${endpoint}/`);
-    const data = await response.json();
-    return data.results;
+async function cacheData(endpoint) {
+    try {
+        let cachedData = JSON.parse(sessionStorage.getItem(endpoint));
+
+        if (cachedData == null || cachedData === undefined) {
+            const response = await fetch(`https://swapi.co/api/${endpoint}/`);
+            const data = await response.json();
+            sessionStorage.setItem(endpoint, JSON.stringify(data.results));
+            return data.results;
+        } else {
+            return cachedData;
+        }
+    } catch (error) {
+        return "error";
+    }
 }
 
 export default CardList;
--- src/index.js
+++ src/index.js
@@ -18,8 +18,6 @@ const theme = createMuiTheme({
     }
 });
 
-console.log(theme);
-
 ReactDOM.render(
     <MuiThemeProvider theme={theme}>
         <App />
